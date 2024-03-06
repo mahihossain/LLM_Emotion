c7bff2227645ff49402200d509f8c292917f2192
Sayed Muddashir Hossain <sayed.muddashir@gmail.com>
2024-03-06 10:10:39 +0000

    Update execution counts and add outputs to notebook

---
+++ Experiments/fine_tuning_lora_llama.py
@@ -0,0 +1,142 @@
+# %%
+# It is based on LoRA paper: https://arxiv.org/abs/2106.09685
+
+import json
+import os
+from pprint import pprint
+import bitsandbytes as bnb
+import torch
+import torch.nn as nn
+import transformers
+from datasets import load_dataset
+from huggingface_hub import notebook_login
+from peft import (
+    LoraConfig,
+    PeftConfig,
+    PeftModel,
+    get_peft_model,
+    prepare_model_for_kbit_training
+)
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    IntervalStrategy,
+)
+
+from trl import SFTTrainer
+
+# %%
+MODEL_NAME = "../models/Llama-2-7b-chat-hf/"
+
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+# %%
+def print_trainable_parameters(model):
+  """
+  Prints the number of trainable parameters in the model.
+  """
+  trainable_params = 0
+  all_param = 0
+  for _, param in model.named_parameters():
+    all_param += param.numel()
+    if param.requires_grad:
+      trainable_params += param.numel()
+  print(
+      f"trainable params: {trainable_params} || all params: {all_param} || trainables%: {100 * trainable_params / all_param}"
+  )
+
+# %%
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+
+# %%
+config = LoraConfig(
+    lora_alpha=16,
+    lora_dropout=0.1,
+    r=8,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+model.modules()
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+# %%
+data = load_dataset("csv", data_files="../data/train_VPN10.csv")
+
+# %%
+data
+
+# %%
+data["train"][0]
+
+# %%
+def generate_prompt(data_point):
+  return f"""
+<human>: {data_point["User"]}
+<context>: {data_point["context"]}
+<assistant>: {data_point["EmotionRegulation1"]}
+""".strip()
+
+def generate_and_tokenize_prompt(data_point):
+  full_prompt = generate_prompt(data_point)
+  tokenized_full_prompt = tokenizer(full_prompt, padding=True, truncation=True)
+  return tokenized_full_prompt
+
+# %%
+data = data["train"].shuffle().map(generate_and_tokenize_prompt)
+
+# %%
+data
+
+# %%
+import warnings
+
+# Proper regular expression to match the warning message
+warnings.filterwarnings("ignore", message="torch.utils.checkpoint: please pass in use_reentrant=True or use_reentrant=False explicitly")
+
+
+# %%
+training_args = transformers.TrainingArguments(
+    output_dir="./saves",
+    num_train_epochs=5,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=1,
+    optim="paged_adamw_32bit",
+    #save_strategy="epoch",
+    save_strategy=IntervalStrategy.STEPS,  # change this line
+    save_total_limit=1,  # add this line
+    logging_strategy="epoch",
+    logging_steps=1,
+    learning_rate=2e-4,
+    weight_decay=0.001,
+    fp16=False,
+    bf16=False,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True,
+    lr_scheduler_type="cosine",
+)
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=data,
+    args=training_args,
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
+)
+model.config.use_cache = False
+trainer.train()
+
+# %%
+model.save_pretrained("./models/llama-2-7b-chat-hf-llm-emo-person-10-finetuned-peft/")
+
+
---
+++ Experiments/testing_prompt_of_fine_tuned_gemma.py
@@ -0,0 +1,126 @@
+# %%
+import torch
+from peft import (
+    PeftConfig,
+    PeftModel
+)
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer
+)
+
+import pandas as pd
+
+from collections import Counter
+from tqdm import tqdm
+
+# %%
+# load the test data
+df = pd.read_csv('../data/test_VPN10.csv')
+
+# %%
+PEFT_MODEL = "./models/gemma-7b-llm-emo-person-10-finetuned-peft/"
+
+config = PeftConfig.from_pretrained(PEFT_MODEL)
+model = AutoModelForCausalLM.from_pretrained(
+    config.base_model_name_or_path,
+    return_dict=True,
+    device_map="auto",
+    trust_remote_code=True
+)
+
+tokenizer=AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = PeftModel.from_pretrained(model, PEFT_MODEL)
+
+# %%
+generation_config = model.generation_config
+generation_config.max_new_tokens = 200
+generation_config.temperature = 0.7
+generation_config.top_p = 0.7
+generation_config.num_return_sequences = 1
+generation_config.pad_token_id = tokenizer.eos_token_id
+generation_config.eos_token_id = tokenizer.eos_token_id
+
+# if cuda is available, use it
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+# prompt = """
+# <human>: midjourney prompt for a boy running in the snow
+# <context>: 
+# <assistant>:
+# """.strip()
+
+# encoding = tokenizer(prompt, return_tensors="pt").to(device)
+# with torch.inference_mode():
+#   outputs = model.generate(
+#       input_ids = encoding.input_ids,
+#       attention_mask = encoding.attention_mask,
+#       generation_config = generation_config
+#   )
+
+# print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+# %%
+emotions = ['REST', 'DEPRECIATION', 'AVOIDANCE', 'STABILIZE_SELF', 'ATTACK_OTHER', 'WITHDRAWAL', 'ATTACK_SELF']
+df_copy = df.copy()
+
+# %%
+emotions
+
+# %%
+# iterate over the dataset, User column is <human> and <context> is the context and <assistant> is the model response
+for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+    prompt = f"""
+    <human>: {row['User']}
+    <context>: {row['context']}
+    <assistant>:
+    """.strip()
+
+    encoding = tokenizer(prompt, return_tensors="pt").to(device)
+    with torch.inference_mode():
+        outputs = model.generate(
+            input_ids = encoding.input_ids,
+            attention_mask = encoding.attention_mask,
+            generation_config = generation_config
+        )
+
+    # Decode the assistant's response
+    assistant_response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    # Extract the part of the assistant's response that comes after '<assistant>'
+    assistant_response = assistant_response.split('<assistant>')[-1].strip().lower()
+
+    # Check if the assistant's response contains a valid emotion
+    emotion_counter = Counter()
+    for emotion in emotions:
+        emotion_counter[emotion] = assistant_response.count(emotion.lower())
+
+    if emotion_counter:
+        # If a valid emotion is found and it's the same as the ground truth, keep the ground truth in df_copy
+        most_common_emotion = emotion_counter.most_common(1)[0][0]
+        if row['EmotionRegulation1'].lower() in [emotion for emotion, count in emotion_counter.items() if count > 0]:
+            df_copy.loc[index, 'EmotionRegulation1'] = row['EmotionRegulation1']
+        else:
+            # If a valid emotion is found but it's not the same as the ground truth, replace the ground truth with the most common emotion in df_copy
+            df_copy.loc[index, 'EmotionRegulation1'] = most_common_emotion
+    else:
+        # If no valid emotion is found, put 'REST' in df_copy
+        df_copy.loc[index, 'EmotionRegulation1'] = 'REST'
+
+# %%
+# Keep only the 'EmotionRegulation1' column and convert it to a DataFrame
+df_copy = df_copy[['EmotionRegulation1']]
+
+# Rename the 'EmotionRegulation1' column to 'Predicted'
+df_copy = df_copy.rename(columns={'EmotionRegulation1': 'Predicted'})
+
+# Add a column 'GroundTruth' with the original 'EmotionRegulation1' column from df
+df_copy['GroundTruth'] = df['EmotionRegulation1']
+
+# Save the df_copy to a csv file as prediction_person_10.csv
+df_copy.to_csv('../data/predictions/gemma_prediction_person_10.csv', index=False)
+
+
