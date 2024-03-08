b4daedb14e0986ccf2c0159df9332126db5c77e0
Sayed Muddashir Hossain <sayed.muddashir@gmail.com>
2024-03-08 12:12:53 +0000

    Refactor tensor multiplication loop and add CUDA availability check

--- Experiments/test_cuda.py
+++ Experiments/test_cuda.py
@@ -4,13 +4,10 @@ import time
 # Check if CUDA is available and set PyTorch to use it
 if torch.cuda.is_available():
     device = torch.device('cuda')
+    print("CUDA is available. Setting device to use CUDA.")
 else:
     print("CUDA is not available. Exiting.")
     exit()
-
-# Create two large tensors
-tensor1 = torch.randn(5000, 5000, device=device)
-tensor2 = torch.randn(5000, 5000, device=device)
 
 start_time = time.time()
 end_time = start_time + 20*60  # 20 minutes from now
@@ -18,10 +15,14 @@ print_time = start_time + 1*60  # 1 minutes from now
 
 # Perform tensor multiplication until approximately 20 minutes have passed
 while time.time() < end_time:
+    # Create two large tensors
+    tensor1 = torch.randn(5000, 5000, device=device)
+    tensor2 = torch.randn(5000, 5000, device=device)
+
     result = torch.matmul(tensor1, tensor2)
 
-    # Print the result every 5 minutes
+    # Print the result every 1 minute
     if time.time() > print_time:
-        print("5 minutes have passed.\n")
+        print("1 minute has passed.\n")
         print(result)
         print_time += 1*60  # Set the next time to print
