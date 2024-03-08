d55ace04232e3ee57026a03952973763a07683b5
Sayed Muddashir Hossain <sayed.muddashir@gmail.com>
2024-03-08 11:39:13 +0000

    Add periodic printing of tensor multiplication result

--- Experiments/test_cuda.py
+++ Experiments/test_cuda.py
@@ -14,10 +14,14 @@ tensor2 = torch.randn(5000, 5000, device=device)
 
 start_time = time.time()
 end_time = start_time + 20*60  # 20 minutes from now
+print_time = start_time + 1*60  # 1 minutes from now
 
 # Perform tensor multiplication until approximately 20 minutes have passed
 while time.time() < end_time:
     result = torch.matmul(tensor1, tensor2)
 
-# Print the result
-print(result)
+    # Print the result every 5 minutes
+    if time.time() > print_time:
+        print("5 minutes have passed.\n")
+        print(result)
+        print_time += 1*60  # Set the next time to print
