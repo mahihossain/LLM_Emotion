7e61ca1be41041cb8a19130960930caf7cf2b1f9
Sayed Muddashir Hossain <sayed.muddashir@gmail.com>
2024-03-09 10:41:54 +0000

    Update generation_config temperature to 0.0

--- Experiments/testing_prompt_of_fine_tuned_llama.py
+++ Experiments/testing_prompt_of_fine_tuned_llama.py
@@ -37,7 +37,7 @@ model = PeftModel.from_pretrained(model, PEFT_MODEL)
 # %%
 generation_config = model.generation_config
 generation_config.max_new_tokens = 200
-generation_config.temperature = 0
+generation_config.temperature = 0.0
 generation_config.top_p = 0.7
 generation_config.num_return_sequences = 1
 generation_config.pad_token_id = tokenizer.eos_token_id
